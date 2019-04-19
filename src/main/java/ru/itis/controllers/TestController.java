b6702902cb56f66de196b62141bc90fca5cf856b
Lesya <sikamikanico9494@gmail.com>
2019-04-19 08:45:20 +0800

    Subscription service added

--- src/main/java/ru/itis/controllers/TestController.java
+++ src/main/java/ru/itis/controllers/TestController.java
@@ -5,15 +5,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import ru.itis.models.Like;
-import ru.itis.models.Message;
-import ru.itis.models.Question;
-import ru.itis.models.User;
+import ru.itis.models.*;
 import ru.itis.repositories.UserRepository;
-import ru.itis.services.LikeService;
-import ru.itis.services.MessageService;
-import ru.itis.services.QuestionService;
-import ru.itis.services.UserService;
+import ru.itis.services.*;
 
 import java.util.Date;
 import java.util.List;
@@ -34,10 +28,15 @@ public class TestController {
     @Autowired
     UserService userService;
 
+    @Autowired
+    SubscriptionService subscriptionService;
+
 
     @RequestMapping(method = RequestMethod.GET, path = "/")
     public String control() {
-        Optional<User> userOptional = userService.authenticate("login", "5f4dcc3b5aa765d61d8327deb882cf99");
+        for (Subscription s: subscriptionService.getSubscriptorsByUser(userService.getUserById(1).get())) {
+            System.out.println(s);
+        }
         return "page";
     }
 
